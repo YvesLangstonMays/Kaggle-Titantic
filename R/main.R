dbafb34a9f9c7365b6d54581be07bb9cf5027657
YvesLangstonMays <yveslangstonmays@gmail.com>
2024-03-13 14:50:07 -0500

    Public Score 74.162%

--- R/main.R
+++ R/main.R
@@ -1,30 +1,27 @@
 library(randomForest)
 
 # Reading the csv file and attaching the data frame
-data = read.csv("titanic_data/train.csv")
-test = read.csv("titanic_data/test.csv")
+data <- read.csv("titanic/train.csv")
+test <- read.csv("titanic/test.csv")
 attach(data)
 
 # Remove columns to improve model
-data$Parch = NULL
-data$Embarked = NULL
-data$Fare = NULL
-data$Age = NULL
-data$Cabin = NULL
+data$Parch <- NULL
+data$Embarked <- NULL
+data$Fare <- NULL
+data$Age <- NULL
 
 # Removing na values
-data = na.omit(data)
+data <- na.omit(data)
 
 # Changing numeric to factor for classification
-data$Survived = factor(data$Survived)
+data$Survived <- factor(data$Survived)
 
 # Random Forest model
-titanic.rfmodel = randomForest(Survived ~ ., ntree = 100000, mtry = 5, data = data)
+titanic.rfmodel <- randomForest(Survived ~ ., ntree = 10000, mtry = 5, data = data)
 titanic.rfmodel
 
-predictions = predict(titanic.rfmodel, newdata = test)
-prediction_results = data.frame(PassengerID = test$PassengerId, Survived = predictions)
+predictions <- predict(titanic.rfmodel, newdata = test)
+prediction_results <- data.frame(PassengerID = test$PassengerId, Survived = predictions)
 
-write.csv(prediction_results, "R/submission.csv", row.names=FALSE)
-
-
+write.csv(prediction_results, "R/submission.csv", row.names = FALSE)
