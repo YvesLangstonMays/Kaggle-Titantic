5bdd1f4082db7053d0a4816163fe3a36c43cbc4f
YvesLangstonMays <yveslangstonmays@gmail.com>
2024-03-28 12:14:59 -0500

    mtry = 8

--- R/main.R
+++ R/main.R
@@ -1,27 +1,31 @@
 library(randomForest)
 
 # Reading the csv file and attaching the data frame
-data <- read.csv("titanic/train.csv")
-test <- read.csv("titanic/test.csv")
+data <- read.csv("titanic_data/train.csv")
+test <- read.csv("titanic_data/test.csv")
 attach(data)
+
+
 
 # Remove columns to improve model
 data$Parch <- NULL
-data$Embarked <- NULL
 data$Fare <- NULL
 data$Age <- NULL
 
 # Removing na values
 data <- na.omit(data)
 
+summary(data)
 # Changing numeric to factor for classification
 data$Survived <- factor(data$Survived)
 
-# Random Forest model
-titanic.rfmodel <- randomForest(Survived ~ ., ntree = 10000, mtry = 5, data = data)
+# Random Forest model, which is just a bootstrapped tree model
+# mtry = 8 nolint
+titanic.rfmodel <- randomForest(Survived ~ ., ntree = 10000, mtry = 8, data = data) # nolint
 titanic.rfmodel
 
 predictions <- predict(titanic.rfmodel, newdata = test)
-prediction_results <- data.frame(PassengerID = test$PassengerId, Survived = predictions)
+prediction_results <- data.frame(PassengerID = test$PassengerId, Survived = predictions) # nolint
+
 
 write.csv(prediction_results, "R/submission.csv", row.names = FALSE)
